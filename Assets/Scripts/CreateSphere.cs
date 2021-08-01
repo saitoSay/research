2575fa19c0d8d8b6b080ef6843b87c9912513299
saitoSay <seinyacchi@gmail.com>
2021-08-01 19:36:01 +0900

    単振動をTransformの値を直接弄って動かしてみた

---
+++ Assets/Scripts/CreateSphere.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSphere : MonoBehaviour
+{
+    [SerializeField] GameObject _sphere;
+    private void Start()
+    {
+        Instantiate(_sphere);
+    }
+}
---
+++ Assets/Scripts/CyclicMotion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CyclicMotion : MonoBehaviour
+{
+    private float _totalTime = 0;
+    Vector3 _pos;
+    public float posX { get; set; }
+    public float posY { get; set; }
+    private void Start()
+    {
+        _pos = new Vector3(0, 0);
+    }
+    private void Update()
+    {
+        _pos.x = SinCyclicMotion();
+        _pos.y = SinCyclicMotion() * CosCyclicMotion();
+        this.transform.position = _pos;
+    }
+    public float SinCyclicMotion()
+    {
+        _totalTime += Time.deltaTime;
+        return Mathf.Sin(_totalTime) * 5;
+    }
+    public float CosCyclicMotion()
+    {
+        _totalTime += Time.deltaTime;
+        return Mathf.Cos(_totalTime) * 5;
+    }
+}
